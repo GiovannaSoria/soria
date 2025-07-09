fbec6eb8a232aecc1ef6016b027f5d5073d4de59
GiovannaSoria <gmsoria@espe.edu.ec>
2025-07-09 17:29:17 -0500

    chore: initial commit

---
+++ src/test/java/com/examen/soria/SoriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examen.soria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
