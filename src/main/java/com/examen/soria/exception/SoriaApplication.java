c666fafcf41d441ef80928e331b31744a5fc03ab
GiovannaSoria <gmsoria@espe.edu.ec>
2025-07-09 19:39:21 -0500

    models

---
+++ src/main/java/com/examen/soria/exception/SoriaApplication.java
@@ -0,0 +1,13 @@
+package com.examen.soria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoriaApplication.class, args);
+	}
+
+}
